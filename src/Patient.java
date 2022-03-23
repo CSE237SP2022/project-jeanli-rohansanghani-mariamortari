f4c89fc22661aca092f16db7b7ece61bbc45b509
mariamortari <mariamortari@marias-mbp.dhcp.wustl.edu>
2022-03-23 11:48:46 -0500

    Create Patient Class

---
+++ src/Patient.java
@@ -0,0 +1,44 @@
+
+public class Patient {
+
+	private String name;
+	private int age;
+	private String sex;
+	private double weight; //in kilograms
+	private double height; //in meters
+	private int patient_id;
+	
+	public Patient(String name, int age, String sex, double weight, double height,int patient_id) {
+		this.name=name;
+		this.age=age;
+		this.sex=sex;
+		this.weight=weight;
+		this.height=height;
+		this.patient_id =patient_id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public String getSex() {
+		return sex;
+	}
+	
+	public double getWeight() {
+		return weight;
+	}
+	
+	public double getHeight() {
+		return height;
+	}
+	
+	public int getPatientId() {
+		return patient_id;
+	}
+
+}
