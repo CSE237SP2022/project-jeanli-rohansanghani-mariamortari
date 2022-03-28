99ed4d715ab615cf927cc0876e26f4c7b4443971
jeanl <jeanl@DESKTOP-URRTL34.localdomain>
2022-03-28 16:49:55 -0500

    Updated the Hosptial Management Class - Added methods to catch mismatched inputs, updated variable names - Still need to add tests

--- src/hospital/HospitalManagementSystem.java
+++ src/hospital/HospitalManagementSystem.java
@@ -1,5 +1,4 @@
 package hospital;
-import java.util.LinkedList;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -19,24 +18,25 @@ public class HospitalManagementSystem {
 		}
 		
 	}
+	
 	public static void menuControls() {
-		Scanner scan = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 		System.out.println("-------------------------------------------------------");
 		System.out.println("Hospital Management System");
 		System.out.println("Select A to Add Patient to the System");
 		System.out.println("Select X to exit Hospital System");
 
 		
-		String str = scan.nextLine();
-		if(str.equals("A")) {
+		String input = in.nextLine();
+		if(input.equals("A")) {
 			addPatient();
 		}
-		if(str.equals("X")) {
+		if(input.equals("X")) {
 			keepGoing = false;
 		}
 		
-		
 	}
+	
 	public static void addPatient() {
 		patientList.add(generatePatient());
 		System.out.println("");
@@ -45,49 +45,115 @@ public class HospitalManagementSystem {
 		printPatientList();
 		
 	}
+	
 	public static void printPatientList() {
 		int counter = 1;
 		for(Patient val: patientList) {
 			if(val!=null) {
-				System.out.println(counter+". ");
-				System.out.println("Patient's Name =   "+""+((Patient) val).getName());
-				System.out.println("Patient's Age =   "+""+((Patient) val).getAge());
-				System.out.println("Patient's Sex =   "+""+((Patient) val).getSex());
-				System.out.println("Patient's Weight (in kilograms) =   "+""+((Patient) val).getWeight());
-				System.out.println("Patient's Height (in meters) =   "+""+((Patient) val).getHeight());
-				System.out.println("Patient's ID =   "+""+((Patient) val).getPatientId());
-				counter+=1;
+				System.out.println(counter + ". ");
+				System.out.println("Patient's Name =   " + "" + ((Patient) val).getFirstName() + "" + ((Patient) val).getLastName());
+				System.out.println("Patient's Age =   " + "" + ((Patient) val).getAge());
+				System.out.println("Patient's Sex =   " + "" + ((Patient) val).getSex());
+				System.out.println("Patient's Weight (in kilograms) =   " + "" + ((Patient) val).getWeight());
+				System.out.println("Patient's Height (in meters) =   " + "" + ((Patient) val).getHeight());
+				System.out.println("Patient's ID =   " + "" + ((Patient) val).getPatientId());
+				counter += 1;
 				System.out.println("");
 				System.out.println("");
 			}
 		}
+	}
+	
+	public static Patient generatePatient() {
+		Scanner in = new Scanner(System.in);
+		
+		String patientFirstName = getFirstNameFromInput(in);
+		
+		String patientLastName = getLastNameFromInput(in);
+		
+		int patientAge = getAgeFromInput(in);
+		
+		String patientSex = getSexFromInput(in);
+		
+		double patientWeight = getWeightFromInput(in);
+		
+		double patientHeight = getHeightFromInput(in);
+		
+		int patientID = getPatientIDFromInput(in);
+		
+		Patient patient = new Patient(patientFirstName,patientLastName,patientAge,patientSex,patientWeight,patientHeight,patientID);
+		
+		return patient;
 		
 	}
-	public static Patient generatePatient() {
-		Scanner scan2 = new Scanner(System.in);
-		
-		System.out.println("Enter Patient's Name");
-		String p1_name = scan2.next();
-		
-		System.out.println("Enter Patient's Age");
-		int p1_age = scan2.nextInt();
-		
+	
+	
+	public static String getFirstNameFromInput(Scanner in) {
+		System.out.println("Enter Patient's First Name");
+		return in.next();
+	}
+	
+	public static String getLastNameFromInput(Scanner in) {
+		System.out.println("Enter Patient's Last Name");
+		return in.next();
+	}
+	
+	public static int getAgeFromInput(Scanner in) {
+		boolean inputAnInt = false;
+		int patientAge = 0;
+		while(!inputAnInt) {
+			System.out.println("Enter Patient's Age");
+			if(in.hasNextInt()) {
+				patientAge = in.nextInt();
+				inputAnInt = true;
+			} else {
+				String response = in.next();
+				System.out.println(response + " is not an integer");
+			}
+		}
+		return patientAge;
+	}
+	
+	public static String getSexFromInput(Scanner in) {
 		System.out.println("Enter Patient's Sex");
-		String p1_sex = scan2.next();
-		
-		System.out.println("Enter Patient's Weight (in kilograms)");
-		double p1_weight = scan2.nextDouble();
-		
-		System.out.println("Enter Patient's Height (in meters)");
-		double p1_height = scan2.nextDouble();
-		
+		return in.next();
+	}
+	
+	public static double getWeightFromInput(Scanner in) {
+		boolean inputANumber = false;
+		double patientWeight = 0;
+		while(!inputANumber) {
+			System.out.println("Enter Patient's Weight (in kilograms)");
+			if(in.hasNextInt()) {
+				patientWeight = in.nextDouble();
+				inputANumber = true;
+			} else {
+				String response = in.next();
+				System.out.println(response + " is not a number");
+			}
+		}
+		return patientWeight;
+	}
+	
+	public static double getHeightFromInput(Scanner in) {
+		boolean inputANumber = false;
+		double patientHeight = 0;
+		while(!inputANumber) {
+			System.out.println("Enter Patient's Height (in meters)");
+			if(in.hasNextInt()) {
+				patientHeight = in.nextDouble();
+				inputANumber = true;
+			} else {
+				String response = in.next();
+				System.out.println(response + " is not a number");
+			}
+		}
+		return patientHeight;
+	}
+	
+	public static int getPatientIDFromInput(Scanner in) {
 		System.out.println("Enter Patient's ID");
-		int p1_id = scan2.nextInt();
-		
-		Patient pt = new Patient(p1_name,p1_age,p1_sex,p1_weight,p1_height,p1_id);
-		
-		return pt;
-		
+		return in.nextInt();
 	}
 	
 	
--- src/test/PatientTest.java
+++ src/test/PatientTest.java
@@ -1,6 +1,8 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import hospital.Patient;
@@ -8,13 +10,23 @@ import hospital.Patient;
 class PatientTest {
 	
 	
-	private Patient p1= new Patient("bob",36,"male",80.0,1.60,13);
+	private Patient p1;
 	
+	@BeforeEach
+	void setup() {
+		p1 = new Patient("bob","jones",36,"male",80.0,1.60,13);
+	}
 	
 	@Test
-	void testGetName() {
-		String name = p1.getName();
-		assertTrue("bob".equals(name));
+	void testGetFirstName() {
+		String firstName = p1.getFirstName();
+		assertTrue("bob".equals(firstName));
+	}
+	
+	@Test
+	void testGetLastName() {
+		String lastName = p1.getLastName();
+		assertTrue("jones".equals(lastName));
 	}
 	
 	@Test
@@ -32,18 +44,18 @@ class PatientTest {
 	@Test 
 	void testGetWeight() {
 		double weight = p1.getWeight();
-		assertEquals(80,weight);
+		assertEquals(80,weight,0.05);
 	}
 	
 	@Test
 	void testGetHeight() {
 		double height = p1.getHeight();
-		assertEquals(1.60,height);
+		assertEquals(1.60,height,0.05);
 	}
 	
 	@Test 
 	void testGetPatientId() {
-		double id = p1.getPatientId();
+		int id = p1.getPatientId();
 		assertEquals(13,id);
 	}
 
